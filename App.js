bf5572ada45f2338d4151b2a697159d018d159f8
atenghamn <alex.tenghamn@gmail.com>
2021-12-14 23:16:03 +0100

    first init

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import Login from './src/components/LogIn';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,9 +6,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gör någonting coolt med detta</Text>
       <StatusBar style="auto" />
+      <Login />
     </View>
+     
   );
 }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+TODO 
+-[] Make the login a modal page
+-[] Make a form to order a job with a rolling picker
+
